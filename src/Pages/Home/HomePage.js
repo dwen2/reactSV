b8b1974ea483c11beec26906fdebcc51a60f1a46
Dezhang <dwen2@byu.edu>
2021-03-24 19:02:19 -0600

    home cards

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,13 +1,11 @@
 import logo from "../../logo.svg";
 import React from "react";
 import Carousel from "../../Carousel/MyCarousel";
+import "./HomePage.css";
 
 function Home() {
   return (
     <div className="App-body">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <p className="midPara">
         <h1 className="midTitle">Nepal Health Stats</h1>
         Nepal Health Stats is a web application that tracks and presents the
@@ -16,8 +14,21 @@ function Home() {
         officially-recognized sources.
       </p>
 
-      <div className="AppCarousel">
-        <Carousel />
+      <hr />
+
+      <h1>
+        Indicator Categories
+      </h1>
+
+      <div>
+        <div class="card bg-primary text-white text-center p-3">
+          <blockquote class="blockquote mb-0">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              posuere erat.
+            </p>
+          </blockquote>
+        </div>
       </div>
     </div>
   );
